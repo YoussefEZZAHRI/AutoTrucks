55c895e4f73a7e57d83d29fb14d8124aa3157da0
YoussefEZZAHRI <y.ezzahri@gmail.com>
2018-08-05 21:31:29 +0000

    fixing JUnit bugs

--- src/test/java/com/piece/MaPieceApplicationTest.java
+++ src/test/java/com/piece/MaPieceApplicationTest.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class MaPieceApplicationTests {
+@SpringBootTest(classes = com.automotiva.MaPieceApplication.class)
+public class MaPieceApplicationTest {
 
 	@Test
 	public void contextLoads() {
